c0f16f4a4611b461f4bd26c8298f59bf1407c1db
PriyanshuBhadauriya <62997069+PriyanshuBhadauriya@users.noreply.github.com>
2022-04-28 11:23:44 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,7 +3,7 @@ const app=express();
 const path =require("path");
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const PORT =process.env.PORT || 80;
+const PORT = 3000;
 
 const db = require("./db");
 const router = require("./routes");
